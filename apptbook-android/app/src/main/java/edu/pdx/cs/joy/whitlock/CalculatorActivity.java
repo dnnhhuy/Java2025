7a9b88d0ef2b755c97b3feed8ae7f4460ae4c5ab
Huy <eddiedoan09@gmail.com>
2025-07-02 10:59:07 +0700

    Add a linear layout for the calculator inputs

--- apptbook-android/app/src/main/java/edu/pdx/cs/joy/whitlock/CalculatorActivity.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs/joy/whitlock/CalculatorActivity.java
@@ -1,6 +1,5 @@
 package edu.pdx.cs.joy.whitlock;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,13 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculator);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void launchCalculator(View view) {
-        Intent intent = new Intent(this, CalculatorActivity.class);
-        startActivity(intent);
+    public void backToMain(View view) {
+        finish();
     }
 }
